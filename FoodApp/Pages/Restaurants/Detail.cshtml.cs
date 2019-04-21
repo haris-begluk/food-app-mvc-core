98e673669c86a9dccaa83a7a5659d03cab079d35
haris-begluk <harisbegluk@gmail.com>
2019-04-22 01:30:09 +0200

    Details pae implemented

---
+++ FoodApp/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodApp.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodApp.Pages.Restaurants
+{
+    public class DetailModel : PageModel
+    {
+        public Restaurant Restaurant { get; set; }
+        public void OnGet(int id)
+        {
+            Restaurant = new Restaurant();
+            Restaurant.Id = id;
+        }
+    }
+}
