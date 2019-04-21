dea1af62fffffb1eeade4f1cd613598c5cd04e72
haris-begluk <harisbegluk@gmail.com>
2019-04-21 14:30:08 +0200

    Add project files.

---
+++ FoodApp/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodApp.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        public IConfiguration _config { get; }
+
+        public ListModel(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void OnGet()
+        {
+            //Message = "Hello world";
+            Message = _config["Message"];
+        }
+    }
+}
