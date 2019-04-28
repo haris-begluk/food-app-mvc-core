fa17937d4caa48eda310049a549ac8a7820a7924
haris-begluk <harisbegluk@gmail.com>
2019-04-28 16:06:49 +0200

    Configuration for EF Core Finished

---
+++ FoodApp.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FoodApp.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
